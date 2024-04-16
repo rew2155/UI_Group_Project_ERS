c985cc67d8c0f5cd4cb5f9f5df99df1895306f25
connorrabinowitz <104035494+connorrabinowitz@users.noreply.github.com>
2024-04-16 01:54:33 -0400

    began setup for quiz template

--- server.py
+++ server.py
@@ -7,12 +7,12 @@ quiz_qs = {
     "1":{
         "id": 1,
         "prompt": "The pile currently looks like this:",
-        "situation":
+        "situation": "<img id='situation' source=''>" #add image file
         "question": "How many extra turns does the following player get?",
-        "answer1": "1",
-        "answer2": "2",
-        "answer3": "3",
-        "answer4": "4",
+        "answer1": "<button id='option1'> 1 </div>",
+        "answer2": "<button id='option2'> 2 </div>",
+        "answer3": "<button id='option3'> 3 </div>",
+        "answer4": "<button id='option4'> 4 </div>",
         "correct_answer": "answer4",
         "next_q": "2",
     },
@@ -20,12 +20,12 @@ quiz_qs = {
     "2":{
         "id": 2,
         "prompt": "The pile currently looks like this:",
-        "situation": 
+        "situation": "<img id='situation' source=''>" #add image file
         "question": "How many extra turns does the following player get?",
-        "answer1": "1",
-        "answer2": "2",
-        "answer3": "3",
-        "answer4": "4",
+        "answer1": "<button id='option1'> 1 </div>",
+        "answer2": "<button id='option2'> 2 </div>",
+        "answer3": "<button id='option3'> 3 </div>",
+        "answer4": "<button id='option4'> 4 </div>",
         "correct_answer": "answer1",
         "next_q": "3",
     },
@@ -33,12 +33,12 @@ quiz_qs = {
     "3":{
         "id": 3,
         "prompt": "The following list provides the sequence of events in a game with 4 players:",
-        "situation": "1. Player 1 plays a 3 <br> 2. Player 2 plays a King <br> 3. Player 3 plays a 2 <br> 4. Player 3 plays a King <br> 5. Player 2 slaps the pile",
+        "situation": "1. Player 1 plays a 3 <br> 2. Player 2 plays a King <br> 3. Player 3 plays a 2 <br> 4. Player 3 plays a King <br> 5. Player 2 slaps the pile", #maybe make this its own div with a border around it
         "question": "What happens next?",
-        "answer1": "Player 2 picks up the pile",
-        "answer2": "Player 2 discards a penalty card",
-        "answer3": "Player 3 picks up the pile",
-        "answer4": "Player 3 discards a penalty card",
+        "answer1": "<button id='option1'> Player 2 picks up the pile </div>",
+        "answer2": "<button id='option2'> Player 2 discards a penalty card </div>",
+        "answer3": "<button id='option3'> Player 3 picks up the pile </div>",
+        "answer4": "<button id='option4'> Player 3 discards a penalty card </div>",
         "correct_answer": "answer1",
         "next_q": "4",
     },
@@ -46,12 +46,12 @@ quiz_qs = {
     "4":{
         "id": 4,
         "prompt": "The following list provides the sequence of events in a game with 3 players:",
-        "situation": "1. Player 1 plays a 4 <br> 2. Player 2 plays a Jack <br> 3. Player 3 plays a Queen <br> 4. Player 1 plays a 2 <br> 5. Player 1 plays a 3",
+        "situation": "1. Player 1 plays a 4 <br> 2. Player 2 plays a Jack <br> 3. Player 3 plays a Queen <br> 4. Player 1 plays a 2 <br> 5. Player 1 plays a 3", #maybe make this its own div with a border around it
         "question": "What happens next?",
-        "answer1": "Player 1 picks up the pile",
-        "answer2": "Player 2 picks up the pile",
-        "answer3": "Player 3 picks up the pile",
-        "answer4": "Player 1 discards a penalty card",
+        "answer1": "<button id='option1'> Player 1 picks up the pile </div>",
+        "answer2": "<button id='option2'> Player 2 picks up the pile </div>",
+        "answer3": "<button id='option3'> Player 3 picks up the pile </div>",
+        "answer4": "<button id='option4'> Player 1 discards a penalty card </div>",
         "correct_answer": "answer3",
         "next_q": "5",
     },
@@ -59,12 +59,12 @@ quiz_qs = {
     "5":{
         "id": 5,
         "prompt": "The following list provides the sequence of events in a game with 2 players:",
-        "situation": "1. Player 1 plays an 8 <br> 2. Player 2 plays a 3 <br> 3. Player 1 plays an 8 <br> 4. Player 2 plays a 7 <br> 5. Player 1 slaps the pile",
+        "situation": "1. Player 1 plays an 8 <br> 2. Player 2 plays a 3 <br> 3. Player 1 plays an 8 <br> 4. Player 2 plays a 7 <br> 5. Player 1 slaps the pile", #maybe make this its own div with a border around it
         "question": "What happens next?",
-        "answer1": "Player 1 picks up the pile",
-        "answer2": "Player 1 discards a penalty card",
-        "answer3": "Player 2 picks up the pile",
-        "answer4": "Player 2 discards a penalty card",
+        "answer1": "<button id='option1'> Player 1 picks up the pile </div>",
+        "answer2": "<button id='option2'> Player 1 discards a penalty card </div>",
+        "answer3": "<button id='option3'> Player 2 picks up the pile </div>",
+        "answer4": "<button id='option4'> Player 2 discards a penalty card </div>",
         "correct_answer": "answer2",
         "next_q": "6",
     },
@@ -72,12 +72,12 @@ quiz_qs = {
     "6":{
         "id": 6,
         "prompt": "It's your turn and the pile currently looks like this:",
-        "situation": 
+        "situation": "<img id='situation' source=''>" #add image file
         "question": "The next card you play is a 7. What do you do next?",
-        "answer1": "You slap the pile",
-        "answer2": "You play another card",
-        "answer3": "You wait for the next player to go",
-        "answer4": "You discard a penalty card",
+        "answer1": "<button id='option1'> You slap the pile </div>",
+        "answer2": "<button id='option2'> You play another card </div>",
+        "answer3": "<button id='option3'> You wait for the next player to go </div>",
+        "answer4": "<button id='option4'> You discard a penalty card </div>",
         "correct_answer": "answer2",
         "next_q": "7",
     },
@@ -85,12 +85,12 @@ quiz_qs = {
     "7":{
         "id": 7,
         "prompt": "It's your turn and the pile currently looks like this:",
-        "situation":
+        "situation": "<img id='situation' source=''>" #add image file
         "question": "The next card you play is a Queen. What do you do next?",
-        "answer1": "You slap the pile",
-        "answer2": "You play another card",
-        "answer3": "You wait for the next player to go",
-        "answer4": "You discard a penalty card",
+        "answer1": "<button id='option1'> You slap the pile </div>",
+        "answer2": "<button id='option2'> You play another card </div>",
+        "answer3": "<button id='option3'> You wait for the next player to go </div>",
+        "answer4": "<button id='option4'> You discard a penalty card </div>",
         "correct_answer": "answer1",
         "next_q": "8",
     },
@@ -98,12 +98,12 @@ quiz_qs = {
     "8":{
         "id": 8,
         "prompt": "It's your turn and the pile currently looks like this:",
-        "situation": 
+        "situation": "<img id='situation' source=''>" #add image file
         "question": "The next card you play is a Jack. What do you do next?",
-        "answer1": "You slap the pile",
-        "answer2": "You play another card",
-        "answer3": "You wait for the next player to go",
-        "answer4": "You discard a penalty card",
+        "answer1": "<button id='option1'> You slap the pile </div>",
+        "answer2": "<button id='option2'> You play another card </div>",
+        "answer3": "<button id='option3'> You wait for the next player to go </div>",
+        "answer4": "<button id='option4'> You discard a penalty card </div>,
         "correct_answer": "answer3",
         "next_q": "9",
     },
@@ -111,12 +111,12 @@ quiz_qs = {
     "9":{
         "id": 9,
         "prompt": "It's your turn and the pile currently looks like this:",
-        "situation": 
+        "situation": "<img id='situation' source=''>" #add image file
         "question": "Drag and drop the card that would provide the next player 1 extra turn.",
-        "answer1": 
-        "answer2":
-        "answer3":
-        "answer4":
+        "answer1": "<img id='option1' source=''>" #add image file
+        "answer2": "<img id='option2' source=''>" #add image file
+        "answer3": "<img id='option3' source=''>" #add image file
+        "answer4": "<img id='option4' source=''>" #add image file
         "correct_answer": "answer1",
         "next_q": "10",
     },
@@ -124,12 +124,12 @@ quiz_qs = {
     "10":{
         "id": 10,
         "prompt": "It's your turn and the pile currently looks like this:",
-        "situation":
+        "situation": "<img id='situation' source=''>" #add image file
         "question": "Drag and drop the card that would create the opportunity to slap a Sandwich.",
-        "answer1":
-        "answer2":
-        "answer3":
-        "answer4":
+        "answer1": "<img id='option1' source=''>" #add image file
+        "answer2": "<img id='option2' source=''>" #add image file
+        "answer3": "<img id='option3' source=''>" #add image file
+        "answer4": "<img id='option4' source=''>" #add image file
         "correct_answer": "answer4",
         "next_q": "score",
     }
---
+++ static/quiz.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    //make it so that for the normal multiple choice questions, each option is clickable
+    //make it so that for the drag and drop questions, each option is draggable and the 'situation' is a droppable zone
+}
+)
