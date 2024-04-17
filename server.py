131e3d72875516fae631180f42abf9228b3338a2
Connor Rabinowitz <car2272@columbia.edu>
2024-04-17 03:02:41 -0400

    created changes to quiz.html, quiz.js, and server.py in order to dynamically change when the quiz has multiple choice vs drag and drop questions

--- server.py
+++ server.py
@@ -8,20 +8,28 @@ quiz_qs = {
     "1": {
         "id": 1,
         "prompt": "The pile currently looks like this:",
-        "situation": "<img src='/static/image1.jpg' alt='Situation Image'>",
+        "situation": "<img class='situation_img' src='https://familymath.stanford.edu/wp-content/uploads/2021/08/playing-cards-e1628288827705.jpg' alt='Situation Image'>",
         "question": "How many extra turns does the following player get?",
-        "answers": [1, 2, 3, 4],
-        "correct_answer": 4,
+        "answer1": "<button id='answer1'>1</button>", 
+        "answer2": "<button id='answer2'>2</button>",
+        "answer3": "<button id='answer3'>3</button>",
+        "answer4": "<button id='answer4'>4</button>",
+        "correct_answer": "answer4",
+        "prev_q": None,
         "next_q": "2"
     },
 
     "2":{
         "id": 2,
         "prompt": "The pile currently looks like this 2:",
-        "situation": "<img id='situation' source='' alt='Situation Image'>", 
+        "situation": "<img id='situation' class='situation_img' src='https://www.wargamer.com/wp-content/sites/wargamer/2022/04/playing-card-games-kemps.jpg' alt='Situation Image'>", 
         "question": "How many extra turns does the following player get?",
-        "answers": [1, 2, 3, 4],
-        "correct_answer": 1,
+        "answer1": "<button id='answer1'>1</button>", 
+        "answer2": "<button id='answer2'>2</button>",
+        "answer3": "<button id='answer3'>3</button>",
+        "answer4": "<button id='answer4'>4</button>",
+        "correct_answer": "answer1",
+        "prev_q": "1",
         "next_q": "3",
     },
 
@@ -34,111 +42,107 @@ quiz_qs = {
                         4. Player 3 plays a King <br> 
                         5. Player 2 slaps the pile""",  # Formatted as a single string
         "question": "What happens next?",
-        "answers": [
-            "Player 2 picks up the pile",
-            "Player 2 discards a penalty card",
-            "Player 3 picks up the pile",
-            "Player 3 discards a penalty card"
-        ],
-        "correct_answer": "Player 2 picks up the pile",
+        "answer1": "<button id='answer1'>Player 2 picks up the pile</button>", 
+        "answer2": "<button id='answer2'>Player 2 discards a penalty card</button>",
+        "answer3": "<button id='answer3'>Player 3 picks up the pile</button>",
+        "answer4": "<button id='answer4'>Player 3 discards a penalty card</button>",
+        "correct_answer": "answer1",
+        "prev_q": "2",
         "next_q": "4",
         },
+
     "4": {
         "id": 4,
         "prompt": "The following list provides the sequence of events in a game with 3 players:",
         "situation": "1. Player 1 plays a 4<br>2. Player 2 plays a Jack<br>3. Player 3 plays a Queen<br>4. Player 1 plays a 2<br>5. Player 1 plays a 3",
         "question": "What happens next?",
-        "answers": [
-            "Player 1 picks up the pile",
-            "Player 2 picks up the pile",
-            "Player 3 picks up the pile",
-            "Player 1 discards a penalty card"
-        ],
-        "correct_answer": "Player 3 picks up the pile",
+        "answer1": "<button id='answer1'>Player 1 picks up the pile</button>", 
+        "answer2": "<button id='answer2'>Player 2 picks up the pile</button>",
+        "answer3": "<button id='answer3'>Player 3 picks up the pile</button>",
+        "answer4": "<button id='answer4'>Player 1 discards a penalty card</button>",
+        "correct_answer": "answer3",
+        "prev_q": "3",
         "next_q": "5",
     },
+
     "5": {
         "id": 5,
         "prompt": "The following list provides the sequence of events in a game with 2 players:",
         "situation": "1. Player 1 plays an 8<br>2. Player 2 plays a 3<br>3. Player 1 plays an 8<br>4. Player 2 plays a 7<br>5. Player 1 slaps the pile",
         "question": "What happens next?",
-        "answers": [
-            "Player 1 picks up the pile",
-            "Player 1 discards a penalty card",
-            "Player 2 picks up the pile",
-            "Player 2 discards a penalty card"
-        ],
-        "correct_answer": "Player 1 discards a penalty card",
+        "answer1": "<button id='answer1'>Player 1 picks up the pile</button>", 
+        "answer2": "<button id='answer2'>Player 1 discards a penalty card</button>",
+        "answer3": "<button id='answer3'>Player 2 picks up the pile</button>",
+        "answer4": "<button id='answer4'>Player 2 discards a penalty card</button>",
+        "correct_answer": "answer2",
+        "prev_q": "4",
         "next_q": "6",
     },
+
     "6": {
         "id": 6,
         "prompt": "It's your turn and the pile currently looks like this:",
-        "situation": "<img src='/static/situation6.jpg' alt='Current pile'>",
+        "situation": "<img class='situation_img' src='https://cdn11.bigcommerce.com/s-sjl48p9/images/stencil/500x659/products/493/477/Jumbo_Deck_of_Cards__00402.1355263732.jpg?c=2' alt='Current pile'>",
         "question": "The next card you play is a 7. What do you do next?",
-        "answers": [
-            "You slap the pile",
-            "You play another card",
-            "You wait for the next player to go",
-            "You discard a penalty card"
-        ],
-        "correct_answer": "You play another card",
+        "answer1": "<button id='answer1'>You slap the pile</button>", 
+        "answer2": "<button id='answer2'>You play another card</button>",
+        "answer3": "<button id='answer3'>You wait for the next player to go</button>",
+        "answer4": "<button id='answer4'>You discard a penalty card</button>",
+        "correct_answer": "answer2",
+        "prev_q": "5",
         "next_q": "7",
     },
+
     "7": {
         "id": 7,
         "prompt": "It's your turn and the pile currently looks like this:",
-        "situation": "<img src='/static/situation7.jpg' alt='Current pile'>",
+        "situation": "<img class='situation_img' src='https://cdn.shopify.com/s/files/1/1788/4029/files/download_12_large.jpg?v=1566685316' alt='Current pile'>",
         "question": "The next card you play is a Queen. What do you do next?",
-        "answers": [
-            "You slap the pile",
-            "You play another card",
-            "You wait for the next player to go",
-            "You discard a penalty card"
-        ],
-        "correct_answer": "You slap the pile",
+        "answer1": "<button id='answer1'>You slap the pile</button>", 
+        "answer2": "<button id='answer2'>You play another card</button>",
+        "answer3": "<button id='answer3'>You wait for the next player to go</button>",
+        "answer4": "<button id='answer4'>You discard a penalty card</button>",
+        "correct_answer": "answer1",
+        "prev_q": "6",
         "next_q": "8",
     },
     "8": {
         "id": 8,
         "prompt": "It's your turn and the pile currently looks like this:",
-        "situation": "<img src='/static/situation8.jpg' alt='Current pile'>",
+        "situation": "<img class='situation_img' src='https://as2.ftcdn.net/v2/jpg/00/80/62/95/1000_F_80629529_zmqcu0Oz8kc0a6PLN97lLexpFWIvG275.jpg' alt='Current pile'>",
         "question": "The next card you play is a Jack. What do you do next?",
-        "answers": [
-            "You slap the pile",
-            "You play another card",
-            "You wait for the next player to go",
-            "You discard a penalty card"
-        ],
-        "correct_answer": "You wait for the next player to go",
+        "answer1": "<button id='answer1'>You slap the pile</button>", 
+        "answer2": "<button id='answer2'>You play another card</button>",
+        "answer3": "<button id='answer3'>You wait for the next player to go</button>",
+        "answer4": "<button id='answer4'>You discard a penalty card</button>",
+        "correct_answer": "answer3",
+        "prev_q": "7",
         "next_q": "9",
     },
     "9": {
         "id": 9,
         "prompt": "It's your turn and the pile currently looks like this:",
-        "situation": "<img src='/static/situation9.jpg' alt='Current pile'>",
+        "situation": "<img id='situation' src='https://lh3.googleusercontent.com/pw/AP1GczNEqnPt9mhOK38n0I-reh-aftn0RMPkffsRB9vpK-J4fUxWZnOMxJjsotzgWCsn8HfCKnAZvaooqU0IcPMtYlaf8rqQUmWt0fWbl5GvUiqHjM_Fmqo=w2400' alt='Current pile'>",
         "question": "Drag and drop the card that would provide the next player 1 extra turn.",
-        "answers": [
-            "<img src='/static/option1.jpg' alt='Option 1'>",
-            "<img src='/static/option2.jpg' alt='Option 2'>",
-            "<img src='/static/option3.jpg' alt='Option 3'>",
-            "<img src='/static/option4.jpg' alt='Option 4'>"
-        ],
-        "correct_answer": "<img src='/static/option1.jpg' alt='Option 1'>",
+        "answer1": "<img id='answer1' class='drag_card' src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/English_pattern_jack_of_spades.svg/1200px-English_pattern_jack_of_spades.svg.png' alt='Option 1'>", 
+        "answer2": "<img id='answer2' class='drag_card' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/51_Q_di_picche.jpg/800px-51_Q_di_picche.jpg' alt='Option 2'>",
+        "answer3": "<img id='answer3' class='drag_card' src='https://m.media-amazon.com/images/I/71EkglvyWjL._AC_UF1000,1000_QL80_.jpg' alt='Option 3'>",
+        "answer4": "<img id='answer4' class='drag_card' src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/English_pattern_ace_of_diamonds.svg/1200px-English_pattern_ace_of_diamonds.svg.png' alt='Option 4'>",
+        "correct_answer": "answer1",
+        "prev_q": "8",
         "next_q": "10",
     },
     "10": {
         "id": 10,
         "prompt": "It's your turn and the pile currently looks like this:",
-        "situation": "<img src='/static/situation10.jpg' alt='Current pile'>",
+        "situation": "<img id='situation' src='https://lh3.googleusercontent.com/pw/AP1GczMCiCQm4x2TuVIgJLTjPHO-rAtJXUlovC2GeU9cOSVwfQV2-zEh6qAs4UMOKOYxitg7pkWRUJFTgsmDtMraj58Q1hDRVMW9LT50q2_ellCGAY5CEW8=w2400' alt='Current pile'>",
         "question": "Drag and drop the card that would create the opportunity to slap a Sandwich.",
-        "answers": [
-            "<img src='/static/option1.jpg' alt='Option 1'>",
-            "<img src='/static/option2.jpg' alt='Option 2'>",
-            "<img src='/static/option3.jpg' alt='Option 3'>",
-            "<img src='/static/option4.jpg' alt='Option 4'>"
-        ],
-        "correct_answer": "<img src='/static/option4.jpg' alt='Option 4'>",
+        "answer1": "<img id='answer1' class='drag_card' src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/5_of_clubs.svg/1200px-5_of_clubs.svg.png' alt='Option 1'>", 
+        "answer2": "<img id='answer2' class='drag_card' src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/7_of_clubs.svg/1200px-7_of_clubs.svg.png' alt='Option 2'>",
+        "answer3": "<img id='answer3' class='drag_card' src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/English_pattern_ace_of_diamonds.svg/1200px-English_pattern_ace_of_diamonds.svg.png' alt='Option 3'>",
+        "answer4": "<img id='answer4' class='drag_card' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/51_Q_di_picche.jpg/800px-51_Q_di_picche.jpg' alt='Option 4'>",
+        "correct_answer": "answer4",
+        "prev_q": "9",
         "next_q": "score",
     }
     }
@@ -392,22 +396,37 @@ def submit_answer(question_id):
         user_answer = request.form.get('answer')
         correct_answer = quiz_qs[question_key]['correct_answer']
 
-        if type(user_answer) == str and type(correct_answer) == int:
-            user_answer = int(user_answer)
-        
-        if user_answer == correct_answer:
-            correct_counter[0] += 1
-        
-        print(correct_counter[0])
+        # Check if the user's answer matches the correct answer
+        is_correct = (user_answer == correct_answer)
 
+        # Increment correct_counter if the answer is correct
+        if is_correct:
+            correct_counter[0] += 1
+
+        # Determine the next question ID
         next_question_id = quiz_qs[question_key]['next_q']
 
+        # Redirect to the next question or results page
         if next_question_id == "score":
             return redirect(url_for('result'))
-        
-        return redirect(url_for('quiz', question_id=next_question_id))
+        else:
+            return jsonify({'is_correct': is_correct, 'redirect': url_for('quiz', question_id=next_question_id)})
     
     return "Invalid request", 400
+
+@app.route('/check-answer/<int:question_id>', methods=['POST'])
+def check_answer(question_id):
+    # Retrieve the submitted answer from the AJAX request
+    submitted_answer = request.form.get('answer')
+
+    # Retrieve the correct answer for the given question_id
+    correct_answer = quiz_qs[str(question_id)]['correct_answer']
+
+    # Check if the submitted answer matches the correct answer
+    if submitted_answer == correct_answer:
+        return 'Correct!'
+    else:
+        return 'Incorrect!'
 
 @app.route('/result')
 def result():
--- static/quiz.js
+++ static/quiz.js
@@ -1,5 +1,131 @@
 $(document).ready(function(){
-    //make it so that for the normal multiple choice questions, each option is clickable
-    //make it so that for the drag and drop questions, each option is draggable and the 'situation' is a droppable zone
-}
-)
+    // Get the current question ID
+    let currentPath = window.location.pathname;
+    let question_id = currentPath.split("/quiz/")[1];
+    question_id = parseInt(question_id);
+
+    let answerDropped = false;
+
+    // Function to handle multiple choice questions
+    function handleMultipleChoice() {
+        // Function to handle click event on answer buttons
+        $(document).on('click', 'button[id^="answer"]', function() {
+            console.log('Button clicked!'); // Debug statement
+
+            // Get the selected answer
+            let selectedAnswer = $(this).attr('id');
+
+            console.log('Selected answer:', selectedAnswer); // Debug statement
+
+            // Disable all answer buttons
+            $('button[id^="answer"]').prop('disabled', true);
+
+            // Make an AJAX call to submit the answer only if the question number is 8 or less
+            if (question_id <= 8) {
+                // Retrieve the CSRF token from the page
+                let csrfToken = $('meta[name=csrf-token]').attr('content');
+
+                console.log('CSRF Token:', csrfToken); // Debug statement
+
+                $.ajax({
+                    url: '/submit-answer/' + question_id,
+                    type: 'POST',
+                    data: { answer: selectedAnswer, csrf_token: csrfToken }, // Include CSRF token in the request data
+                    success: function(response) {
+                        // Display the result in the "result" span
+                        if (response.is_correct) {
+                            $('#result').text('Correct!');
+                        } else {
+                            $('#result').text('Incorrect!');
+                        }
+                    },
+                    error: function(xhr, status, error) {
+                        // Handle error
+                        console.error(error);
+                    }
+                });
+            }
+        });
+    }
+
+    // Function to handle drag-and-drop questions
+    function handleDragAndDrop() {
+        // Enable draggable for answer images if no answer has been dropped yet
+        if (!answerDropped) {
+            $('img[id^="answer"]').draggable({
+                revert: 'invalid',
+                cursor: 'move',
+                opacity: 0.7
+            });
+        }
+
+        // Enable droppable for the situation image
+        $('#situation').droppable({
+            drop: function(event, ui) {
+                // Get the dropped answer image ID
+                let droppedAnswer = ui.draggable.attr('id');
+
+                console.log('Dropped answer:', droppedAnswer); // Debug statement
+
+                // Set answerDropped flag to true
+                answerDropped = true;
+
+                console.log('Answer dropped:', answerDropped); // Debug statement
+
+                // Disable dragging of answer choices
+                $('img[id^="answer"]').draggable('disable');
+
+                // Remove the last part of the URL after /quiz/1
+                let baseUrl = window.location.href.split('/quiz/')[0];
+
+                // Make AJAX call to check if dropped answer is correct
+                $.ajax({
+                    url: baseUrl + '/check-answer/' + question_id, // Pass the current question ID dynamically
+                    type: 'POST',
+                    data: { answer: droppedAnswer },
+                    success: function(response) {
+                        // Display result message based on server response
+                        $('#result').text(response);
+                    },
+                    error: function(xhr, status, error) {
+                        console.error('AJAX error:', error);
+                    }
+                });
+            }
+        });
+    }
+
+    // Function to handle different question formats based on question ID
+    function handleQuestionFormat() {
+        if (question_id < 9) {
+            handleMultipleChoice();
+        } else {
+            // Logic for handling different question format (e.g., drag and drop)
+            handleDragAndDrop();
+        }
+    }
+
+    // Call the function to handle the question format
+    handleQuestionFormat();
+
+    // Function to handle navigation to the previous question
+    $('#prev').click(function() {
+        let prevQuestionID = question_id - 1;
+        if (prevQuestionID > 0) {
+            window.location.href = '/quiz/' + prevQuestionID;
+        } else {
+            window.location.href = window.location.href.split('/quiz/')[0];
+        }
+    });
+
+    // Function to handle navigation to the next question
+    $('#next').click(function() {
+        let nextQuestionID = question_id + 1;
+        if (nextQuestionID <= 10) { // Assuming there are 10 questions in total
+            window.location.href = '/quiz/' + nextQuestionID;
+        } else {
+            window.location.href = '/result';
+        }
+    });
+    
+});
